01f69308064e1351282576b3b04945bdf7835b36
Lakshmi prasanna <lakshmiprasa22@gmail.com>
2024-12-19 16:16:19 +0530

    modified

--- python.py
+++ python.py
@@ -1,5 +1,8 @@
 age = int(input("Enter age: "))
 if age >= 18:
+    print("you are a major")
     print("you are eligible to vote")
 else:
+    print("you are minor")
+    print("go home and sleep")
     print("you are not eligible to vote")
